e9333b395021709a61df20cfda56539651b6da16
zkpursuit <zkpursuit@yeah.net>
2019-01-22 11:37:38 +0800

    同步派发事件，事件处理器中使用线程和FutureTask获取异步执行结果。

--- src/com/kaka/notice/Command.java
+++ src/com/kaka/notice/Command.java
@@ -8,7 +8,7 @@ import com.kaka.util.ObjectPool;
  * @author zkpursuit
  */
 abstract public class Command extends Notifier implements ObjectPool.Poolable {
-    
+
     /**
      * 注册时的命令号
      */
@@ -34,6 +34,11 @@ abstract public class Command extends Notifier implements ObjectPool.Poolable {
 
     /**
      * 处理消息，此方法中不要用线程处理
+     * <br>
+     * 由于{@link com.kaka.notice.Facade}中调度事件时，此方法被执行后即刻
+     * 对本对象进行池化处理，将调用reset方法；如内部再使用线程，将引发不必要的错误。
+     * <br>
+     * 未池化的对象因不会调用reset方法，内部可使用线程。
      *
      * @see Message Message处理方法
      * @param msg 通知消息
--- test/kaka/Asyn_Test.java
+++ test/kaka/Asyn_Test.java
@@ -34,7 +34,7 @@ public class Asyn_Test extends Startup {
             4、异步future模式获取事件处理结果其本质是利用wait、notify（notifyAll）
         实现，而使用事件模式则无需调用wait让线程中断等待。
          */
-        //future模式获取异步处理结果
+        //获取异步处理结果
         Message asynMsg = new Message("10000", "让ResultCommand接收执行");
         //由于事件通知为广播模式，故而必须为执行结果进行命名标识唯一性
         IResult<String> result0 = asynMsg.setResult("ResultMsg", new AsynResult<>());
@@ -47,5 +47,11 @@ public class Asyn_Test extends Startup {
         IResult<String> result1 = syncMsg.setResult("ResultMsg", new SyncResult<>());
         facade.sendMessage(syncMsg, false);  //同步发送事件通知
         System.out.println(result1.get());
+        
+        //另一种异步处理方式,同步派发事件，事件处理器中使用FutureTask及线程异步获取执行结果
+        Message syncMsg1 = new Message("30000", "让FutureCommand接收执行");
+        IResult<String> result2 = syncMsg1.setResult("ResultMsg", new SyncResult<>());
+        facade.sendMessage(syncMsg1, false); //同步发送事件通知
+        System.out.println(result2.get());
     }
 }
